f4796b36ae4e432d5e94d82155ef7ee6632137c6
Barret <barret.obrock@gmail.com>
2019-10-12 07:22:41 -0500

    Add version, date updated properties

--- slacktools/__init__.py
+++ slacktools/__init__.py
@@ -5,3 +5,7 @@ Slack Tools: A common library for working with Slack
 """
 
 from .tools import SlackTools
+from ._version import get_versions
+__version__ = get_versions()['version']
+__update_date__ = get_versions()['date']
+del get_versions
