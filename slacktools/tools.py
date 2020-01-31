773666e16da74c68f8d6d85e664132189a537ccc
bobrock <barret.obrock@gmail.com>
2020-01-31 13:04:42 -0600

    Remove kavalkilu dependencies

--- slacktools/tools.py
+++ slacktools/tools.py
@@ -3,15 +3,40 @@
 import os
 import re
 import time
+import json
 import string
-import signal
+import pygsheets
 import requests
 from tabulate import tabulate
 from slack import WebClient
 from random import randint
 from datetime import datetime as dt
 from datetime import timedelta as tdelta
-from kavalkilu import GSheetReader, Log
+
+
+class GSheetReader:
+    """A class to help with reading in Google Sheets"""
+    def __init__(self, sheet_key):
+        with open(os.path.join(os.path.expanduser('~'), *['keys', 'GSHEET_READER'])) as f:
+            gsheets_creds = json.loads(f.read())
+        os.environ['GDRIVE_API_CREDENTIALS'] = json.dumps(gsheets_creds)
+        self.gc = pygsheets.authorize(service_account_env_var='GDRIVE_API_CREDENTIALS')
+        self.sheets = self.gc.open_by_key(sheet_key).worksheets()
+
+    def get_sheet(self, sheet_name):
+        """Retrieves a sheet as a pandas dataframe"""
+        for sheet in self.sheets:
+            if sheet.title == sheet_name:
+                return sheet.get_as_df()
+        raise ValueError(f'The sheet name "{sheet_name}" was not found '
+                         f'in the list of available sheets: ({",".join([x.title for x in self.sheets])})')
+
+    def write_df_to_sheet(self, sheet_key, sheet_name, df):
+        """Write df to sheet"""
+        wb = self.gc.open_by_key(sheet_key)
+        sheet = wb.worksheet_by_title(sheet_name)
+        sheet.clear()
+        sheet.set_dataframe(df, (1, 1))
 
 
 class SlackTools:
@@ -29,7 +54,7 @@ class SlackTools:
             e.g., emoji uploads
             default: empty
         """
-        self.log = Log(log_name, child_name='slacktools')
+        # self.log = Log(log_name, child_name='slacktools')
         # Enforce lowercase triggers (regex will be indifferent to case anyway
         if triggers is not None:
             triggers = list(map(str.lower, triggers))
@@ -60,14 +85,14 @@ class SlackTools:
             if matches.group(1).lower() in self.triggers:
                 # Matched using abbreviated triggers
                 trigger = matches.group(1).lower()
-                self.log.debug('Matched on abbreviated trigger: {}'.format(trigger))
+                # self.log.debug('Matched on abbreviated trigger: {}'.format(trigger))
             else:
                 # Matched using bot id
                 trigger = matches.group(2)
-                self.log.debug('Matched on bot id: {}'.format(trigger))
+                # self.log.debug('Matched on bot id: {}'.format(trigger))
             message_txt = matches.group(3).lower().strip()
             raw_message = matches.group(3).strip()
-            self.log.debug('Message: {}'.format(message_txt))
+            # self.log.debug('Message: {}'.format(message_txt))
             return trigger, message_txt, raw_message
         return None, None, None
 
