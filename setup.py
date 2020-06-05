0b836d08db55fb748526281e46e2fac1a7da97d4
barretobrock <barret.obrock@gmail.com>
2020-06-04 19:43:35 -0500

    Break out requirements to requirements.txt, implement new structure to setup

--- setup.py
+++ setup.py
@@ -4,31 +4,37 @@
 Setup Slacktools.
 """
 import os
+import yaml
 import versioneer
 from setuptools import setup, find_packages
 
+# Read in package info via YAML
+with open('config.yaml') as f:
+    package_info = yaml.load(f, Loader=yaml.FullLoader)
 
+PACKAGE = package_info['REPO']['NAME']
+DESC = package_info['REPO']['DESC']
+URL = package_info['REPO']['URL']
 here_dir = os.path.abspath(os.path.dirname(__file__))
-init_fp = os.path.join(here_dir, *['slacktools', '__init__.py'])
+init_fp = os.path.join(here_dir, *[PACKAGE, '__init__.py'])
+
+# Package Requirements
+fpath = os.path.join(here_dir, 'requirements.txt')
+with open(fpath, 'r') as f:
+    reqs_raw = f.read()
+reqs_list = reqs_raw.strip().split('\n')
 
 setup_args = {
-    'name': 'slacktools',
+    'name': PACKAGE,
     'version': versioneer.get_version(),
     'cmdclass': versioneer.get_cmdclass(),
-    'license': 'MIT',
-    'description': 'A common library for working with Slack',
-    'url': 'https://github.com/barretobrock/slacktools',
+    'license': 'GPL',
+    'description': DESC,
+    'url': URL,
     'author': 'Barret Obrock',
     'author_email': 'barret@barretobrock.ee',
     'packages': find_packages(exclude=['tests']),
-    'install_requires': [
-        'Flask==1.1.2',
-        'pyee==7.0.1',
-        'pygsheets==2.0.3.1',
-        'requests>=2.20.0'
-        'slackclient==2.6.1',
-        'tabulate==0.8.7',
-    ],
+    'install_requires': reqs_list
 }
 
 setup(**setup_args)
